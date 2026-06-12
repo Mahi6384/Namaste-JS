c2a210231e5a7c30fd8f38677d84b6299a50092a
Mahi Jain <mahij.2912@gmail.com>
2026-06-12 14:15:32 +0530

    EP-06: add Toggle component with conditional rendering

--- reactprep/prep/src/App.js
+++ reactprep/prep/src/App.js
@@ -4,28 +4,64 @@ import React from "react";
 import Comp1 from "./Comp1";
 import { useState } from "react";
 
-function Counter() {
-    const [count, setCount] = useState(0);
+// function Counter() {
+//     const [count, setCount] = useState(0);
 
+//     return (
+//         <div>
+//             <p>{count}</p>
+//             <button onClick={() => setCount(count + 1)}>+</button>
+//             <button onClick={() => setCount(count - 1)}>-</button>
+//             <button onClick={() => setCount(0)}>Reset</button>
+//         </div>
+//     );
+// }
+
+// function InputBox() {
+//     const [text, setText] = useState("");
+
+//     function handleChange(event) {
+//         setText(event.target.value);
+//     }
+//     const remaining = 300 - text.length;
+//     const countercolor = remaining < 20 ? "red" : "black";
+//     return (
+//         <>
+//             <textarea
+//                 style={{ height: "300px", width: "300px", marginTop: "30px" }}
+//                 value={text}
+//                 placeholder="Start"
+//                 onChange={handleChange}
+//             />
+//             <p style={{ color: countercolor }}>remaining char = {remaining}</p>
+//         </>
+//     );
+// }
+
+function Toggle() {
+    const [show, setShow] = useState(false);
+    function handleClick() {
+        setShow(!show);
+    }
     return (
         <div>
-            <p>{count}</p>
-            <button onClick={() => setCount(count + 1)}>+</button>
-            <button onClick={() => setCount(count - 1)}>-</button>
-            <button onClick={() => setCount(0)}>Reset</button>
+            <button onClick={handleClick}>
+                {show ? "Hide the Secret" : "Show the Secret"}
+            </button>
+            {/* <p> {show ? "HELLO" : ""}</p> */}
+            {show && <p>Hello</p>}
         </div>
     );
 }
 
-function InputBox() {
-    return <input style = {{width:'200px', height:100px}} placeholder="Start Typing"></input>;
-}
 function App() {
     return (
         <div className="App">
-            <h1>Ehjkbfh</h1>
-            <Comp1 name="Mahi" age={[1, 2, 5]} />
-            <Counter />
+            {/* <h1>Ehjkbfh</h1> */}
+            {/* <Comp1 name="Mahi" age={[1, 2, 5]} /> */}
+            {/* <Counter /> */}
+            {/* <InputBox /> */}
+            <Toggle />
         </div>
     );
 }
