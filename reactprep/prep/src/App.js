50a50dec1a5d59d275bc375c02187efb0da0cb47
Mahi Jain <mahij.2912@gmail.com>
2026-06-15 14:19:16 +0530

    EP-06: practiced state lifting with synced slider and input

--- reactprep/prep/src/App.js
+++ reactprep/prep/src/App.js
@@ -38,30 +38,65 @@ import { useState } from "react";
 //     );
 // }
 
-function Toggle() {
-    const [show, setShow] = useState(false);
-    function handleClick() {
-        setShow(!show);
-    }
+// function Toggle() {
+//     const [show, setShow] = useState(false);
+//     function handleClick() {
+//         setShow(!show);
+//     }
+//     return (
+//         <div>
+
+//             <button onClick={handleClick}>
+//                 {show ? "Hide the Secret" : "Show the Secret"}
+//             </button>
+//             {/* <p> {show ? "HELLO" : ""}</p> */}
+//             {show && <p>Hello</p>}
+//         </div>
+//     );
+// }
+
+function Slider({ onChange, val }) {
     return (
         <div>
-            <button onClick={handleClick}>
-                {show ? "Hide the Secret" : "Show the Secret"}
-            </button>
-            {/* <p> {show ? "HELLO" : ""}</p> */}
-            {show && <p>Hello</p>}
+            <input
+                type="range"
+                min="0"
+                max="100"
+                step="1"
+                value={val}
+                onChange={onChange}
+            />
         </div>
     );
 }
 
+function Input({ onChange, val }) {
+    return (
+        <input
+            type="number"
+            placeholder="write temperature"
+            onChange={onChange}
+            value={val}
+        />
+    );
+}
 function App() {
+    const [val, setVal] = useState(0);
+
+    function onChange(event) {
+        setVal(Number(event.target.value));
+    }
     return (
         <div className="App">
             {/* <h1>Ehjkbfh</h1> */}
             {/* <Comp1 name="Mahi" age={[1, 2, 5]} /> */}
             {/* <Counter /> */}
             {/* <InputBox /> */}
-            <Toggle />
+            {/* <Toggle /> */}
+
+            <Slider onChange={onChange} val={val} />
+            <Input onChange={onChange} val={val} />
+            <p>{val}</p>
         </div>
     );
 }
--- reactprep/prep/src/Comp1.js
+++ reactprep/prep/src/Comp1.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 export default function Comp1(props) {
     {
         console.log(props);
